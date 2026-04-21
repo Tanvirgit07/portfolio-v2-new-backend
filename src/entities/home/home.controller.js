47396dcd7210a8aec0b49ca4378c9350f56531ce
Tanvir Ahmmed <ahmmedtanvir009@gmail.com>
2026-04-21 14:51:18 +0600

    add

--- src/entities/home/home.controller.js
+++ src/entities/home/home.controller.js
@@ -1,19 +1,3 @@
-export const registerUser = async (req, res, next) => {
-  const { name, email, password } = req.body;
-  try {
-
-    const data = await registerUserService({ name, email, password });
-    generateResponse(res, 201, true, 'Registered user successfully!', data);
-  }
-
-  catch (error) {
-
-    if (error.message === 'User already registered.') {
-      generateResponse(res, 400, false, 'User already registered', null);
-    }
-
-    else {
-      next(error)
-    }
-  }
-};
+export const createHome = async (req,res,next) => {
+const {  } = req.body;
+}
